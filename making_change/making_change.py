55be802b48ae68cf66e4a1edf12c5c53e2c4a115
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-06-17 19:03:42 -0600

    making change done and tested

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,52 +3,21 @@
 import sys
 
 def making_change(amount, denominations):
-  coins = [0, 0, 0, 0, 0]
-  count_list = set()
-
-  def helper_function(amount, coins, count_list, denom):
-
-    if amount - (coins[0]*denom[0] + coins[1]*denom[1] + coins[2]*denom[2] + 
-                 coins[3]*denom[3] + coins[4]*denom[4]) >= denom[4]:
-      coins_50 = coins.copy()
-      coins_50[4] += 1
-      helper_function(amount, coins_50, count_list, denom)
-
-    if amount - (coins[0]*denom[0] + coins[1]*denom[1] + coins[2]*denom[2] + 
-                 coins[3]*denom[3] + coins[4]*denom[4]) >= denom[3]:
-      coins_25 = coins.copy()
-      coins_25[3] += 1
-      helper_function(amount, coins_25, count_list, denom)
-
-    if amount - (coins[0]*denom[0] + coins[1]*denom[1] + coins[2]*denom[2] + 
-                 coins[3]*denom[3] + coins[4]*denom[4]) >= denom[2]:
-      coins_10 = coins.copy()
-      coins_10[2] += 1
-      helper_function(amount, coins_10, count_list, denom)
-
-    if amount - (coins[0]*denom[0] + coins[1]*denom[1] + coins[2]*denom[2] + 
-                 coins[3]*denom[3] + coins[4]*denom[4]) >= denom[1]:
-      coins_5 = coins.copy()
-      coins_5[1] += 1
-      helper_function(amount, coins_5, count_list, denom)
-
-    if amount - (coins[0]*denom[0] + coins[1]*denom[1] + coins[2]*denom[2] + 
-                 coins[3]*denom[3] + coins[4]*denom[4]) >= denom[0]:
-      coins_1 = coins.copy()
-      coins_1[0] += 1
-      helper_function(amount, coins_1, count_list, denom)
-
-    if amount - (coins[0]*denom[0] + coins[1]*denom[1] + coins[2]*denom[2] + 
-                 coins[3]*denom[3] + coins[4]*denom[4]) == 0:
-      count_list.add(tuple(coins))
-
-    #print(count_list)
-
-    return len(count_list)
-
-  count = helper_function(amount, coins, count_list, denominations)
-
-  return count
+  if amount < 0:
+    return 0
+  elif amount == 0:
+    return 1
+  else:
+    total = 0
+    for i in range(len(denominations)):
+      change = denominations[i]
+      if change > amount:
+        continue
+      else:
+        result = making_change(amount - change, denominations[i:])
+        if result > 0:
+          total += result
+    return total
 
 
 if __name__ == "__main__":
